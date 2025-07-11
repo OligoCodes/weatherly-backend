32735a1a99d3fb0a8989c746934b13ca55b190e1
Joseph Kwabena Osei Bonsu <josephoseibonsu742@gmail.com>
2025-07-11 09:39:06 +0000

    Update weather.js

--- weather.js
+++ weather.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'https://weatherly-nine-pi.vercel.app',
+  origin: 'https://weatherly-nine-pi.vercel.app/',
   credentials: true
 }));
 
